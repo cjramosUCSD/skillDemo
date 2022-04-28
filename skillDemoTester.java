5d0bd96f63d71d7879e9db52f2a3f5438e897850
cjramosUCSD <103283539+cjramosUCSD@users.noreply.github.com>
2022-04-27 19:13:07 -0700

    all files created

---
+++ skillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTester {
+    @Test
+    public void multiply(){
+        assertEquals(3,skillDemo.multiplication(1, 2));
+    }
+}
